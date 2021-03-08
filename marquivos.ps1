d41fb70e405326196f27517bae34d9f8f9d125ca
Eduarda Aguiar <73914795+eaguiara@users.noreply.github.com>
2021-03-08 12:38:55 -0300

    Add files via upload

---
+++ marquivos.ps1
@@ -0,0 +1,10 @@
+﻿#selecionar qual pasta procurar arquivo
+$arquivos = (Get-ChildItem -Path "C:\Users\eange\OneDrive\Documentos\marquivos").FullName
+
+#informar o ultimo arquivo
+foreach ($file in $arquivos)
+{
+   $lastWrite = (Get-Item $file).LastWriteTime
+}
+#listar os arquivos
+Get-ChildItem -Path C:\Users\eange\OneDrive\Documentos\marquivos –Recurse
